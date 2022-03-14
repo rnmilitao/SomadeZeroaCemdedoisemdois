99d8309b82789abd6afb176a9bb75e45294518ce
Rozicleide <roziibr@hotmail.com>
2022-03-14 16:19:36 -0400

    Add project files.

---
+++ SomadeZeroaCemdedoisemdois/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomadeZeroaCemdedoisemdois
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int soma = 0;
+            for (int i = 0; i <= 100; i += 2) 
+            {
+                soma += i;
+            }
+            Console.WriteLine(soma);
+            Console.ReadKey();
+
+
+        }
+    }
+}
